1e289933fda657d617f35ff398f107ac9557d305
Stef Korporaal <stefkorporaal@gmail.com>
2019-12-17 20:45:10 +0100

    Latest

--- sol/cli/stodo/stodo.py
+++ sol/cli/stodo/stodo.py
@@ -1,17 +1,22 @@
 # TODO More commands: append, prepend, separate check/uncheck, remove, archive
 # TODO Support for done archive
+# TODO consistent support for indices (make sscs a subclass of tasklist with)
+# TODO header and footer?
 import argparse
+import itertools
 import logging
-from pathlib import Path
-from typing import Dict
 from collections import namedtuple
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Tuple
 
 import sol
 
+from ...task import Task
 from ...tasklist import TaskList
-from ..subparser import AccumulatingSubparserAction
 
 LOG = logging.getLogger(__name__)
+
+Subparsers = Any
 
 
 COMMAND_REGISTER = []
@@ -19,11 +24,11 @@ COMMAND_REGISTER = []
 Command = namedtuple("Command", ["name", "type", "aliases"])
 
 
-def register_command(type_, aliases):
-    def wrapper(method):
-        command = Command(
-            name=method.__name__, type=type_, aliases=list(aliases)
-        )
+def register_command(aliases: List[str] = None, type: str = None) -> Callable:
+    aliases = aliases and list(aliases)
+
+    def wrapper(method: Callable) -> Callable:
+        command = Command(name=method.__name__, type=type, aliases=aliases)
         COMMAND_REGISTER.append(command)
         return method
 
@@ -31,21 +36,15 @@ def register_command(type_, aliases):
 
 
 class STodo:
-    COMMAND_MAP: Dict[str, str] = {}
+    COMMAND_MAP: Dict[str, str] = {"print": "print"}
 
-    DEFAULT_PATHS = (Path.home(), sol.LOG_FOLDER.parent / "doc")
-    DEFAULT_NAMES = ("todo", "to-do", "todo-test")
+    DEFAULT_PATHS: List[Path] = [Path.home(), sol.LOG_FOLDER.parent / "doc"]
+    DEFAULT_NAMES: List[str] = ["todo", "to-do", "todo-test"]
 
-    def __init__(self, filename=None):
-        self.tasklist = None
-        self.parser = self.create_parser()
-        self.args = self.parser.parse_args()
+    MODES: List[str] = ["todotxt", "sscs"]
 
-        sol.configure_logger(self.args.verbosity)
-
-        self.modified = False
-
-        locations = [
+    def __init__(self, filename: Path = None) -> None:
+        locations: List[Path] = [
             # path / (name + ".todo.txt")
             path / (name + ".txt")
             for path in self.DEFAULT_PATHS
@@ -57,40 +56,82 @@ class STodo:
 
         for location in locations:
             try:
-                self.tasklist = TaskList.from_file(location)
+                tasklist = TaskList.from_file(location)
+                self.filename = location
                 break
             except FileNotFoundError:
                 pass
         else:
-            raise FileNotFoundError()
+            # TODO? Create new file here?
+            raise FileNotFoundError("Unable to find valid file!")
 
+        self.parser, args = self.parse_args()
+        sol.configure_logger(args.verbosity)
+        self.settings = self.read_settings(tasklist, args)
+
+        self.modified = False
+
+        self.taskdict = self.get_taskdict(tasklist)
+
+    def get_taskdict(self, tasklist: TaskList) -> Dict[int, Task]:
+        index = itertools.count(1)
+        if self.settings.mode == "sscs":
+            taskdict = {
+                next(index): task
+                for task in tasklist
+                if task.keywords.get("c") != "header"
+                and task.keywords.get("c") != "footer"
+            }
+
+        else:
+            taskdict = {next(index): task for task in tasklist}
+
+        return taskdict
+
+    #
+    # Settings
+    #
     @staticmethod
-    def populate_parser(parser, type_):
+    def read_settings(
+        tasklist: TaskList, args: argparse.Namespace
+    ) -> argparse.Namespace:
+        if args.mode == "":
+            try:
+                mode = tasklist[0].keywords.get("mode", "")
+                if mode:
+                    args.mode = mode
+            except IndexError:
+                pass
+
+        if not args.mode:
+            args.mode = "todotxt"
+
+        return args
+
+    #
+    # Argument Parser
+    #
+    @staticmethod
+    def populate_parser(
+        parser: argparse.ArgumentParser, type_: str
+    ) -> argparse.ArgumentParser:
         if type_ is None:
             return parser
 
         if type_ == "index":
             parser.add_argument("selection", nargs=1, type=int)
-
-        elif type_ == "sort":
-            parser.add_argument(
-                "--format",
-                "-f",
-                choices=["sscs"],
-                help="sort according to format",
-            )
+        else:
+            raise ValueError(f'Unknown parser type "{type_}"!')
 
         return parser
 
     @classmethod
-    def create_subparsers(cls, subparsers, parents=None):
-        parents = parents or []
-
+    def create_subparsers(cls, subparsers: Subparsers) -> Subparsers:
         parsers = []
 
         for command in COMMAND_REGISTER:
             parser = subparsers.add_parser(
-                command.name, aliases=command.aliases, parents=parents
+                command.name, aliases=command.aliases
             )
             for alias in [command.name] + command.aliases:
                 if alias in cls.COMMAND_MAP:
@@ -106,9 +147,10 @@ class STodo:
         return parsers
 
     @classmethod
-    def create_common_parser(cls):
-        common_parser = argparse.ArgumentParser(add_help=False)
-        common_parser.add_argument(
+    def parse_args(cls) -> Tuple[argparse.ArgumentParser, argparse.Namespace]:
+        root_parser = argparse.ArgumentParser()
+
+        root_parser.add_argument(
             "--verbose",
             "-v",
             action="count",
@@ -117,104 +159,104 @@ class STodo:
             help="increase verbosity",
         )
 
-        common_parser.add_argument(
-            "--hide-tags",
-            "-ht",
+        root_parser.add_argument(
+            "--skip-tags",
+            "-st",
             action="store_true",
             help="hide keyword tags",
         )
 
-        return common_parser
-
-    @classmethod
-    def create_parser(cls):
-        common_parser = cls.create_common_parser()
-
-        root_parser = argparse.ArgumentParser(parents=[common_parser])
-        subparsers = root_parser.add_subparsers(
-            dest="subcommand", action=AccumulatingSubparserAction
+        root_parser.add_argument(
+            "--mode",
+            "-m",
+            choices=cls.MODES,
+            default="",
+            help="set alternate modes",
         )
 
-        cls.create_subparsers(subparsers, [common_parser])
+        subparsers = root_parser.add_subparsers(dest="command",)
 
-        return root_parser
+        cls.create_subparsers(subparsers)
 
-    def get_index(self):
-        index = self.args.selection[0] - 1
+        args, extra = root_parser.parse_known_args()
+        args = root_parser.parse_args(extra, args)
+        args.command = args.command or ""
 
-        if not 0 <= index < len(self.tasklist):
+        return root_parser, args
+
+    #
+    # Support functions
+    #
+    def get_index(self) -> int:
+        index = self.settings.selection[0]
+
+        if not 0 <= index < len(self.taskdict):
             self.parser.error(
-                f"Index must be in range 1 < i <= {len(self.tasklist)}"
+                f"Index must be in range 1 < i <= {len(self.taskdict)}"
             )
 
         return index
 
-    def run_command(self, name):
-        try:
-            return getattr(self, self.COMMAND_MAP[name])()
-        except KeyError:
-            if name:
-                self.parser.error(f'Unknown command: "{name}"')
+    def run_command(self, name: str) -> Any:
+        if name:
+            for title in (f"{name}_{self.settings.mode}", name):
+                try:
+                    out = getattr(self, self.COMMAND_MAP[title])()
+                    LOG.info(f"Running command {title}")
+                    return out
+                except AttributeError:
+                    LOG.warning(f"Unable to find function {title}")
+                except KeyError:
+                    LOG.warning(f"Unable to find {title} in COMMAND_MAP")
 
-    @register_command("index", ["check", "do", "tick"])
-    def done(self):
+            LOG.info(f"Unable to find '{name}'")
+        else:
+            LOG.info("Command is empty, so only printing")
+
+    #
+    # Commands
+    #
+    @register_command(["check", "do", "tick"], "index")
+    def done(self) -> None:
         index = self.get_index()
 
-        self.tasklist[index].complete = (
-            "" if self.tasklist[index].complete else "x"
+        self.taskdict[index].complete = (
+            "" if self.taskdict[index].complete else "x"
         )
 
         self.modified = True
 
-    @register_command("sort", ["order"])
-    def sort(self):
-        # Custom ordering (by project, by context, by keyword)
-        format_ = self.args.format
+    @register_command(["order"])
+    def sort(self) -> None:
+        pass
 
-        # Pre-sort
-        self.tasklist = self.tasklist.order()
-        # Post-sort
+    @register_command(["delete"], "index")
+    def remove(self) -> None:
+        pass
 
-        if format_ == "sscs":
-            headers = [
-                task
-                for task in self.tasklist
-                if task.keywords.get("c", "") == "header"
-            ]
-            footers = [
-                task
-                for task in self.tasklist
-                if task.keywords.get("c", "") == "footer"
-            ]
+    def print(self, print_index=True) -> None:
+        for index in sorted(self.taskdict.keys()):
+            line = self.taskdict[index].to_string(self.settings.skip_tags)
+            if print_index:
+                print(f"{index}: {line}")
+            else:
+                print(line)
 
-            for task in headers + footers:
-                self.tasklist.remove(task)
-
-            self.tasklist = TaskList(
-                headers + self.tasklist + footers,
-                filename=self.tasklist.filename,
-            )
-
-        self.modified = True
-
-    @register_command("index", ["delete"])
-    def remove(self):
-        index = self.get_index()
-
-        # TODO Prompt for removal
-        # TODO? Keep history?
-        _ = self.tasklist.pop(index)
-
-        self.modified = True
-
-    def main(self):
-        print(self.args)
-        self.run_command(self.args.subcommand)
-
-        print(self.tasklist.to_string(True, self.args.hide_tags))
+    def main(self) -> None:
+        print(self.settings)
+        self.run_command(self.settings.command)
+        self.run_command("print")
 
         if self.modified:
-            self.tasklist.to_file()
+            tasklist = TaskList(
+                [
+                    self.taskdict[index]
+                    for index in sorted(self.taskdict.keys())
+                ],
+                filename=self.filename,
+            )
+
+            tasklist.to_file()
 
 
 def main():
