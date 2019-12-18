16cf2872d792022dbe2344ba4b6c566fc69b0eba
Stef Korporaal <stefkorporaal@gmail.com>
2019-12-18 16:12:37 +0100

    New CLIApp

---
+++ sol/cli/cliapp.py
@@ -0,0 +1,167 @@
+import argparse
+import logging
+from collections import namedtuple
+from typing import Any, Callable, Dict, List, Union
+
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.NullHandler())
+
+COMMAND_REGISTER = []
+
+Command = namedtuple("Command", ["name", "kwargs", "aliases"])
+
+
+class Argument:
+    def __init__(self, *names, default=None, nargs=None):
+        self.names = names
+        self.default = default
+        self.nargs = nargs
+
+    def __repr__(self):
+        string = ", ".join(
+            f"{attr}={getattr(self, attr)!r}"
+            for attr in dir(self)
+            if not attr.startswith("_")
+        )
+        return f"{type(self).__name__}({string})"
+
+
+class Flag(Argument):
+    def __init__(self, *names, default=None):
+        super().__init__(*names, default=default, nargs=0)
+
+
+def register_command(*aliases, kwargs: List[Argument] = None) -> Callable:
+    aliases = aliases or []  # type: ignore
+    kwargs = kwargs or []
+
+    def wrapper(method: Callable) -> Callable:
+        command = Command(
+            name=method.__name__, kwargs=kwargs, aliases=list(aliases)
+        )
+        COMMAND_REGISTER.append(command)
+        return method
+
+    return wrapper
+
+
+class CLIApp:
+    COMMAND_MAP: Dict[str, str] = {"default": "default"}
+
+    def __init__(self, common_options=None) -> None:
+        self.parser = self.create_argparser(common_options)
+        args = self.parse_args(common_options)
+
+        LOG.debug(f"COMMAND_MAP:\n{self.COMMAND_MAP}")
+
+        self.settings = self.read_settings(args)
+
+    @staticmethod
+    def read_settings(args: argparse.Namespace) -> argparse.Namespace:
+        return args
+
+    @staticmethod
+    def populate_parser(
+        parser: argparse.ArgumentParser, arguments: List[Union[Flag]]
+    ) -> argparse.ArgumentParser:
+        for arg in arguments:
+            LOG.debug("Adding Argument %r", arg)
+            if isinstance(arg, Flag):
+                parser.add_argument(
+                    *arg.names,
+                    action="store_true"
+                    if arg.default is False
+                    else "store_false",
+                )
+            elif isinstance(arg, Argument):
+                parser.add_argument(
+                    *arg.names, default=arg.default or None, nargs=arg.nargs,
+                )
+
+        return parser
+
+    @classmethod
+    def create_subparsers(cls, subparsers: Any) -> Any:
+        parsers = []
+
+        for command in COMMAND_REGISTER:
+            parser = subparsers.add_parser(
+                command.name, aliases=command.aliases
+            )
+            for alias in [command.name] + command.aliases:
+                if alias in cls.COMMAND_MAP:
+                    raise ValueError(
+                        f"{alias} is already registered to "
+                        f"{cls.COMMAND_MAP[alias]}!"
+                    )
+                cls.COMMAND_MAP[alias] = command.name
+
+            parser = cls.populate_parser(parser, command.kwargs)
+            parsers.append(parser)
+
+        return parsers
+
+    def parse_args(self, common_options):
+        args, extra = self.parser.parse_known_args()
+        args = common_options.parse_args(extra, args)
+        args.command = args.command or ""
+
+        return args
+
+    @classmethod
+    def create_argparser(cls, common_options=None) -> argparse.ArgumentParser:
+        parents = common_options and [common_options]
+        root_parser = argparse.ArgumentParser(parents=parents)
+
+        subparsers = root_parser.add_subparsers(dest="command")
+
+        cls.create_subparsers(subparsers)
+
+        return root_parser
+
+    def run_command(self, name: str) -> Any:
+        name = name or "default"
+        try:
+            func = getattr(self, self.COMMAND_MAP[name])
+            return func()
+
+        except AttributeError:
+            LOG.warning(f"Unable to find function {name}")
+        except KeyError:
+            LOG.warning(f"Unable to find {name} in COMMAND_MAP")
+
+        raise ValueError(f"Unable to run function {name}!")
+
+    # def run_command(self, name: str) -> Any:
+    #     if name:
+    #         for title in (f"{name}_{self.settings.mode}", name):
+    #             try:
+    #                 func = getattr(self, self.COMMAND_MAP[title])
+    #                 break
+
+    #             except AttributeError:
+    #                 LOG.warning(f"Unable to find function {title}")
+    #             except KeyError:
+    #                 LOG.warning(f"Unable to find {title} in COMMAND_MAP")
+    #         else:
+    #             LOG.info(f"Unable to find '{name}'")
+
+    #         LOG.info(f"Running command {title}")
+    #         return func()
+
+    #     else:
+    #         LOG.info("Command is empty, so only printing")
+
+    def main(self):
+        self.pre_command()
+        self.run_command(self.settings.command)
+        self.post_command()
+
+    def default(self):
+        pass
+
+    def pre_command(self):
+        pass
+
+    def post_command(self):
+        pass
--- sol/cli/stodo.py
+++ sol/cli/stodo.py
@@ -1,49 +1,30 @@
 # TODO More commands: append, prepend, separate check/uncheck, remove, archive
 # TODO Support for done archive
-# TODO consistent support for indices (make sscs a subclass of tasklist with)
-# TODO header and footer?
+# TODO Settings support
 import argparse
 import logging
-from collections import namedtuple
-from pathlib import Path
-from typing import Any, Callable, Dict, List, Tuple
 from datetime import datetime
+from pathlib import Path
+from typing import List
 
 import sol
 
-from ..tasklist import TaskList, SSCS
+from ..tasklist import SSCS, TaskList
+from .cliapp import Argument, CLIApp, register_command
 
 LOG = logging.getLogger(__name__)
 
-Subparsers = Any
 
-
-COMMAND_REGISTER = []
-
-Command = namedtuple("Command", ["name", "type", "aliases"])
-
-
-def register_command(aliases: List[str] = None, type: str = None) -> Callable:
-    aliases = aliases or []
-    aliases = aliases and list(aliases)
-
-    def wrapper(method: Callable) -> Callable:
-        command = Command(name=method.__name__, type=type, aliases=aliases)
-        COMMAND_REGISTER.append(command)
-        return method
-
-    return wrapper
-
-
-class STodo:
-    COMMAND_MAP: Dict[str, str] = {}
-
+class STodo(CLIApp):
     DEFAULT_PATHS: List[Path] = [Path.home(), sol.LOG_FOLDER.parent / "doc"]
     DEFAULT_NAMES: List[str] = ["todo", "to-do", "todo-test"]
 
     MODES: List[str] = ["todotxt", "sscs"]
 
     def __init__(self, filename: Path = None) -> None:
+        super().__init__(self.common_options())
+        sol.configure_logger(self.settings.verbosity)
+
         locations: List[Path] = [
             # path / (name + ".todo.txt")
             path / (name + ".txt")
@@ -65,81 +46,16 @@ class STodo:
             # TODO? Create new file here?
             raise FileNotFoundError("Unable to find valid file!")
 
-        self.parser, args = self.parse_args()
-
-        sol.configure_logger(args.verbosity)
-        LOG.debug(f"COMMAND_MAP:\n{self.COMMAND_MAP}")
-
-        self.settings = self.read_settings(self.tasklist, args)
-
         if self.settings.mode == "sscs":
             self.tasklist = SSCS(self.tasklist)
 
         self.modified = False
 
-    #
-    # Settings
-    #
-    @staticmethod
-    def read_settings(
-        tasklist: TaskList, args: argparse.Namespace
-    ) -> argparse.Namespace:
-        if args.mode == "":
-            try:
-                mode = tasklist[1].keywords.get("mode", "")
-                if mode:
-                    args.mode = mode
-            except IndexError:
-                pass
-
-        if not args.mode:
-            args.mode = "todotxt"
-
-        return args
-
-    #
-    # Argument Parser
-    #
-    @staticmethod
-    def populate_parser(
-        parser: argparse.ArgumentParser, type_: str
-    ) -> argparse.ArgumentParser:
-        if type_ is None:
-            return parser
-
-        if type_ == "index":
-            parser.add_argument("selection", nargs=1, type=int)
-        else:
-            raise ValueError(f'Unknown parser type "{type_}"!')
-
-        return parser
-
     @classmethod
-    def create_subparsers(cls, subparsers: Subparsers) -> Subparsers:
-        parsers = []
+    def common_options(cls):
+        common_options = argparse.ArgumentParser(add_help=False)
 
-        for command in COMMAND_REGISTER:
-            parser = subparsers.add_parser(
-                command.name, aliases=command.aliases
-            )
-            for alias in [command.name] + command.aliases:
-                if alias in cls.COMMAND_MAP:
-                    raise ValueError(
-                        f"{alias} is already registered to "
-                        f"{cls.COMMAND_MAP[alias]}!"
-                    )
-                cls.COMMAND_MAP[alias] = command.name
-
-            parser = cls.populate_parser(parser, command.type)
-            parsers.append(parser)
-
-        return parsers
-
-    @classmethod
-    def parse_args(cls) -> Tuple[argparse.ArgumentParser, argparse.Namespace]:
-        root_parser = argparse.ArgumentParser()
-
-        root_parser.add_argument(
+        common_options.add_argument(
             "--verbose",
             "-v",
             action="count",
@@ -148,43 +64,22 @@ class STodo:
             help="increase verbosity",
         )
 
-        root_parser.add_argument(
-            "--skip-tags",
-            "-st",
-            action="store_true",
-            help="hide keyword tags",
+        common_options.add_argument(
+            "--skip-tags", "-st", action="store_true",
         )
 
-        root_parser.add_argument(
-            "--mode",
-            "-m",
-            choices=cls.MODES,
-            default="",
-            help="set alternate modes",
+        common_options.add_argument(
+            "--mode", "-m", choices=cls.MODES, default="todotxt",
         )
 
-        subparsers = root_parser.add_subparsers(dest="command",)
+        return common_options
 
-        cls.create_subparsers(subparsers)
-
-        args, extra = root_parser.parse_known_args()
-        args = root_parser.parse_args(extra, args)
-        args.command = args.command or ""
-
-        return root_parser, args
-
-    #
-    # Support functions
-    #
     def update_footer(self):
         if self.tasklist.footers:
             for task in self.tasklist.footers:
                 msg = " ".join(task.msg.split()[-2:])
                 try:
-                    datetime.strptime(
-                        msg,
-                        "%Y-%m-%d %H:%M:%S.%f"
-                    )
+                    datetime.strptime(msg, "%Y-%m-%d %H:%M:%S.%f")
                 except ValueError:
                     continue
 
@@ -194,7 +89,7 @@ class STodo:
                 break
 
     def get_index(self) -> int:
-        index = self.settings.selection[0]
+        index = int(self.settings.index[0])
 
         if not 1 <= index <= len(self.tasklist):
             self.parser.error(
@@ -203,30 +98,12 @@ class STodo:
 
         return index
 
-    def run_command(self, name: str) -> Any:
-        if name:
-            for title in (f"{name}_{self.settings.mode}", name):
-                try:
-                    func = getattr(self, self.COMMAND_MAP[title])
-                    break
-
-                except AttributeError:
-                    LOG.warning(f"Unable to find function {title}")
-                except KeyError:
-                    LOG.warning(f"Unable to find {title} in COMMAND_MAP")
-            else:
-                LOG.info(f"Unable to find '{name}'")
-
-            LOG.info(f"Running command {title}")
-            return func()
-
-        else:
-            LOG.info("Command is empty, so only printing")
-
     #
     # Commands
     #
-    @register_command(["check", "do", "tick"], "index")
+    @register_command(
+        "check", "do", "tick", "index", kwargs=[Argument("index")]
+    )
     def done(self) -> None:
         index = self.get_index()
 
@@ -236,27 +113,25 @@ class STodo:
 
         self.modified = True
 
-    @register_command(["order"])
-    def sort(self) -> None:
-        pass
+    # @register_command("order")
+    # def sort(self) -> None:
+    #     pass
 
-    @register_command(["delete"], "index")
-    def remove(self) -> None:
-        pass
+    # @register_command("delete", kwargs=[Argument("index")])
+    # def remove(self) -> None:
+    #     pass
 
     @register_command()
     def print(self) -> None:
         print(self.tasklist.to_string(True, self.settings.skip_tags))
 
-    def main(self) -> None:
-        print(self.settings)
-        self.run_command(self.settings.command)
-        self.run_command("print")
+    def post_command(self):
+        self.print()
+
+        if self.settings.mode == "sscs":
+            self.update_footer()
 
         if self.modified:
-            if self.settings.mode == "sscs":
-                self.update_footer()
-
             self.tasklist.to_file()
 
 
