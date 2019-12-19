9cb084aed704b277ac6f827fdbfa836a2a3c145c
Stef Korporaal <stefkorporaal@gmail.com>
2019-12-19 23:44:24 +0100

    New stuff

--- sol/cli/stodo.py
+++ sol/cli/stodo.py
@@ -1,16 +1,14 @@
 # TODO More commands: append, prepend, separate check/uncheck, remove, archive
 # TODO Support for done archive
-# TODO Settings support
 # TODO Colorize output
 # TODO? Do validation on options?
 import argparse
 import logging
 from datetime import datetime
 from pathlib import Path
-from typing import List
 from types import SimpleNamespace
+from typing import List, Optional
 
-import cliapp
 import sol
 from cliapp.cliapp import CLIApp
 from cliapp.register import Register
@@ -38,19 +36,15 @@ class STodo(CLIApp):
             if filename is not None:
                 self.filename = Path(filename)
                 if not self.filename.exists():
-                    self.parser.error(
-                        f'Unable to open "{Path(filename)}"!'
-                    )
+                    self.parser.error(f'Unable to open "{Path(filename)}"!')
             else:
-                self.filename = Path(self.settings.todofile)
+                self.filename = Path(self.settings.todo_file)
 
             self.tasklist = TaskList.from_file(Path.expanduser(self.filename))
 
         except (AttributeError, FileNotFoundError):
             files = [
-                path
-                for path in self.config_dir.iterdir()
-                if path.is_file()
+                path for path in self.config_dir.iterdir() if path.is_file()
             ]
             for file in files:
                 if str(file).endswith(".todo.txt"):
@@ -63,7 +57,7 @@ class STodo(CLIApp):
 
             else:
                 raise FileNotFoundError(
-                    "Can't find text file! Please set todofile in cfg."
+                    "Can't find text file! Please set todo-file in cfg."
                 ) from None
 
         self.settings = self.read_settings_from_tasklist()
@@ -76,50 +70,42 @@ class STodo(CLIApp):
             self.tasklist = SolTaskList(self.tasklist)
 
         self.modified: bool = False
+        self.print_list: Optional[TaskList] = None
 
+    #################################################
+    #    Parsing arguments and reading settings     #
+    #################################################
     def handle_early_args(self):
-        # TODO Catch exception and "play it back" with self.parser
-        # TODO? Integrate with common_options?
-        # TODO? Add first two options to common_parser, parse_known_args, add
-        # TODO? rest of options, pass on to cliapp
-        early_parser = argparse.ArgumentParser()
-        early_parser.add_argument("--todo-file", "-t")
-        early_parser.add_argument(
-            "--verbose",
-            "-v",
-            action="count",
-            default=0,
-            dest="verbosity",
-            help="increase verbosity",
-        )
-
+        early_parser = self.get_common_options(1)
         args, remaining_args = early_parser.parse_known_args()
-
         sol.configure_logger(args.verbosity)
-        cliapp.configure_logger(args.verbosity, sol.LOG_FOLDER)
 
         return args.todo_file, remaining_args
 
     @classmethod
-    def get_common_options(cls):
+    def get_common_options(cls, stage=2):  # pylint: disable=arguments-differ
         common_options = argparse.ArgumentParser(add_help=False)
 
-        common_options.add_argument(
-            "--verbose",
-            "-v",
-            action="count",
-            default=0,
-            dest="verbosity",
-            help="increase verbosity",
-        )
+        if stage >= 1:
+            common_options.add_argument(
+                "--verbose",
+                "-v",
+                action="count",
+                default=0,
+                dest="verbosity",
+                help="increase verbosity",
+            )
 
-        common_options.add_argument(
-            "--skip-tags", "-st", action="store_true",
-        )
+            common_options.add_argument("--todo-file", "-t")
 
-        common_options.add_argument(
-            "--mode", "-m", choices=cls.MODES, default=argparse.SUPPRESS
-        )
+        if stage >= 2:
+            common_options.add_argument(
+                "--skip-tags", "-st", action="store_true",
+            )
+
+            common_options.add_argument(
+                "--mode", "-m", choices=cls.MODES, default=argparse.SUPPRESS
+            )
 
         return common_options
 
@@ -144,6 +130,9 @@ class STodo(CLIApp):
 
         return self.settings
 
+    ###############################
+    #    Support for commands     #
+    ###############################
     def update_footer(self):
         # TODO? Use dateutil to parse times dynamically
         time_fmt = "%Y-%m-%d %H:%M:%S.%f"
@@ -168,7 +157,7 @@ class STodo(CLIApp):
                 break
 
     def get_index(self) -> int:
-        index = int(self.settings.index[0])
+        index = int(self.get_setting("index"))
 
         if not 1 <= index <= len(self.tasklist):
             self.parser.error(
@@ -177,9 +166,9 @@ class STodo(CLIApp):
 
         return index
 
-    #
-    # Commands
-    #
+    ###################
+    #    Commands     #
+    ###################
     @Register.command("check", "do", "tick", "index")
     @Register.argument("index")
     def done(self) -> None:
@@ -193,16 +182,51 @@ class STodo(CLIApp):
     @Register.command()
     @Register.argument("task")
     @Register.argument("--date", "-d", action="store_true")
+    @Register.argument("--index", "-i", default=None)
     def add(self):
-        task = Task.from_string(self.settings.task)
-        if self.settings.date:
+        task = Task.from_string(self.get_setting("task"))
+        if self.get_setting("date"):
             task.date_created = datetime.now()
         print(task)
         print(repr(task))
 
-    # @register_command("order")
-    # def sort(self) -> None:
-    #     pass
+        # TODO? Prompt or require flag before appending?
+
+        if self.get_setting("index") is not None:
+            self.tasklist.insert(self.get_index() - 1, task)
+
+        else:
+            self.tasklist.append(task)
+
+        self.modified = True
+
+    @Register.command("order")
+    def sort(self) -> None:
+        self.tasklist.sort()
+        self.modified = True
+
+    @Register.command("search")
+    @Register.argument("query")
+    @Register.argument(
+        "--by", '-by',
+        choices=["project", "context", "keyword", "message", "msg"],
+        dest="target",
+        default="message",
+    )
+    @Register.argument("--strict", action="store_true")
+    @Register.argument("--case-sensitive", "-cs", action="store_true")
+    def filter(self):
+        # TODO Highlight searches
+        query = self.get_setting("query")
+        target = self.get_setting("target")
+        if target == "message":
+            target = "msg"
+        strict = self.get_setting("strict", bool)
+        case_sens = self.get_setting("case_sensitive", bool)
+
+        self.print_list = self.tasklist.filter_by(
+            query, target, strict, case_sens
+        )
 
     # @register_command("delete", kwargs=[Argument("index")])
     # def remove(self) -> None:
@@ -210,14 +234,18 @@ class STodo(CLIApp):
 
     @Register.command()
     def print(self) -> None:
+        tasklist = self.print_list or self.tasklist
         print(f"{self.filename}:")
-        print(self.tasklist.to_string(True, self.settings.skip_tags))
+        print(tasklist.to_string(True, self.get_setting("skip_tags")))
 
     def post_command(self):
         self.print()
 
         if self.settings.mode == "sol":
             self.update_footer()
+
+        if self.get_setting("keep_sorted", bool) and self.tasklist.sort():
+            self.modified = True
 
         if self.modified:
             self.tasklist.to_file()
