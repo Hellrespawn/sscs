cee5e910556f7f14a7e3f69ea54cdcc64236f460
Stef Korporaal <stefkorporaal@gmail.com>
2019-12-20 21:19:27 +0100

    Fix

--- sol/__init__.py
+++ sol/__init__.py
@@ -6,8 +6,8 @@ __version_info__ = tuple(
     int(i) for i in __version__.split(".") if i.isdigit()
 )
 
-LOG = logging.getLogger()
+LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.NullHandler())
 
 LOG_FORMAT = "{levelname:.1} [{name}:{lineno:>04}]: {message}"
-LOG_PATH = Path(Path(__file__).parents[1], "log")
+LOG_PATH = Path(__file__).parents[1] / "log"
