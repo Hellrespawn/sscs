d044cccb02821c79b78bb0743dd00c26c90d51a2
Stef Korporaal <stefkorporaal@gmail.com>
2019-12-29 18:14:06 +0100

    Working test base

--- tests/test_stodo.py
+++ tests/test_stodo.py
@@ -1,24 +1,39 @@
 import logging
-import textwrap
 import unittest
 from unittest.mock import mock_open, patch
 
 import sol
 from loggingextra import configure_logger
-from sol.cli.stodo import STodo
+from sol.cli.stodo.stodo import STodo
+from sol.task import Task
 
-LOG = logging.getLogger("sol." + __name__)
+LOG = logging.getLogger()
 
-TODOFILE = textwrap.dedent(
-    """
-    (A) This is a +project at @place
-    (A) This is also +project at @place
-    (B) This is also +project at @place
-    """
-)
+TODOFILE = """
+header:options mode:sol This is a test
+(B) c:TODO +sol @cli/stodo_old.py ln:001 More commands: append, prepend, separate check/uncheck, remove, archive
+(B) c:TODO +sol @cli/stodo_old.py ln:003 Colorize output
+(B) c:TODO +sol @cli/stodo_old.py ln:005 Multiple indices at the same time
+(B) c:TODO +sol @cli/stodo_old.py ln:356 Highlight searches
+(B) c:TODO +tests @test_stodo.py ln:012 FILE = textwrap.dedent(
+(B) c:TODO +tests @test_stodo.py ln:021 FILE)
+(C) c:TODO? +sol @cli/sscs.py ln:002 append instead of overwrite flag
+(C) c:TODO? +sol @cli/stodo_old.py ln:004 Do validation on options?
+(C) c:TODO? +sol @cli/stodo_old.py ln:004 Do validation on options?
+(C) c:TODO? +sol @cli/stodo_old.py ln:222 Use dateutil to parse times dynamically
+(C) c:TODO? +sol @cli/stodo_old.py ln:262 Delete file if empty?
+(C) c:TODO? +sol @cli/stodo_old.py ln:317 Prompt or require flag before appending?
+(C) c:TODO? +sol @task.py ln:001 Check completed date after created date
+footer:time Generated on 2019-12-29 13:15:08.031440
+""".strip()
+
+DONEFILE = """
+x (B) c:TODO +sol @cli/stodo_old.py ln:002 Support for done archive
+x (B) c:TODO +sol @cli/stodo_old.py ln:245 Implement prompt()
+x (C) c:TODO? +sol @cli/sscs.py ln:001 Try opening gitignore
+""".strip()
 
 
-@patch("builtins.open", new_callable=mock_open, read_data=TODOFILE)
 class TestArgs(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -26,16 +41,47 @@ class TestArgs(unittest.TestCase):
         self.longMessage = True
         self.prefix = "stodo.py"
 
-    def base_arg_test(self, args):
-        with patch("sys.argv", [self.prefix] + args):
-            STodo().main()
+    def base_arg_test(self, args, todo=None, done=None):
+        with patch("sys.argv", [self.prefix] + ["-vvvv"] + args.split()):
+            app = STodo()
+            todo = todo if todo is not None else TODOFILE.split("\n")
+            done = done if done is not None else DONEFILE.split("\n")
 
-    def test_no_args(self, mocked_open):
-        self.base_arg_test([])
+            app.todo = [Task.from_string(ln) for ln in todo]
+            app.done = [Task.from_string(ln) for ln in done]
 
-    def test_unknown_args(self, mocked_open):
+            with patch("builtins.open", new_callable=mock_open) as fakefile:
+                app.main()
+                LOG.debug(fakefile)
+
+    def test_no_args(self):
+        self.base_arg_test("")
+
+    def test_unknown_args(self):
         with self.assertRaises(SystemExit):
-            self.base_arg_test(["unknown"])
+            self.base_arg_test("unknown")
+
+    def test_append_no_index(self):
+        with self.assertRaises(ValueError):
+            self.base_arg_test("append")
+
+    def test_append_no_tasks(self):
+        with self.assertRaises(ValueError):
+            self.base_arg_test("append 1", todo=[])
+
+    def test_append(self):
+        self.base_arg_test("append 1 Appending test!")
+
+    def test_prepend_no_index(self):
+        with self.assertRaises(ValueError):
+            self.base_arg_test("prepend")
+
+    def test_prepend_no_tasks(self):
+        with self.assertRaises(ValueError):
+            self.base_arg_test("prepend 1", todo=[])
+
+    def test_prepend(self):
+        self.base_arg_test("prepend 1 prepending test!")
 
 
 if __name__ == "__main__":
